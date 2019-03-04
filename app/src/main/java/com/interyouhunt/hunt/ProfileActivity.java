ef9f88855b51bfe6d4fa5420294a21398ebf3d31
faisalgedi <faisalgedi@gmail.com>
2019-03-04 01:08:23 -0500

    screen change on button

--- app/src/main/java/com/interyouhunt/hunt/ProfileActivity.java
+++ app/src/main/java/com/interyouhunt/hunt/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.interyouhunt.hunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -23,5 +26,14 @@ public class ProfileActivity extends AppCompatActivity {
         TextView phone = findViewById(R.id.userPhone);
         String userPhone  = "678-974-9193";
         phone.setText(userPhone);
+
+        Button myButton = findViewById(R.id.back);
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
