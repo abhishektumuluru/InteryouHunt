ef6841ebdc7a6d138243dc9928011109b3d43b89
abhishektumuluru <abhishek.tumuluru123@gmail.com>
2019-03-03 19:08:50 -0500

    Initial commit with login screen and other empty screens

---
+++ app/src/main/java/com/interyouhunt/hunt/AddCompanyActivity.java
@@ -0,0 +1,13 @@
+package com.interyouhunt.hunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_company);
+    }
+}
