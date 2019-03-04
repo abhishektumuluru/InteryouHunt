e8f3aa9df409be5d05e71e8064f13332d0d1c206
abhishektumuluru <abhishek.tumuluru123@gmail.com>
2019-03-04 01:01:40 -0500

    Initial commit with login screen and other empty screens

--- app/src/main/java/com/interyouhunt/hunt/LoginActivity.java
+++ app/src/main/java/com/interyouhunt/hunt/LoginActivity.java
@@ -1,13 +1,85 @@
 package com.interyouhunt.hunt;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button loginButton;
+    private FirebaseAuth mAuth;
+
+    private Button registrationButton;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailField = findViewById(R.id.input_email);
+        passwordField = findViewById(R.id.input_password);
+        loginButton = findViewById(R.id.btn_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInUser();
+            }
+        });
+
+        registrationButton = findViewById(R.id.btn_register);
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRegistrationActivity();
+            }
+
+
+        });
+    }
+
+    protected void signInUser() {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Please enter your username and password.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "There was a problem signing in.", Toast.LENGTH_LONG).show();
+                } else {
+                    startHomeActivity();
+                }
+
+            }
+        });
+    }
+    private void startHomeActivity() {
+        this.startActivity(new Intent(this, HomeActivity.class));
+    }
+
+    private void startRegistrationActivity() {
+        this.startActivity(new Intent(this, RegistrationActivity.class));
+
     }
 }
