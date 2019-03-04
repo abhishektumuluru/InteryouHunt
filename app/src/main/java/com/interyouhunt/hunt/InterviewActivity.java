fb1e84cd3e7217256cfb0a66bb3b7b484a431b01
faisalgedi <faisalgedi@gmail.com>
2019-03-04 00:11:52 -0500

    ui changes to profile

--- app/src/main/java/com/interyouhunt/hunt/InterviewActivity.java
+++ app/src/main/java/com/interyouhunt/hunt/InterviewActivity.java
@@ -2,12 +2,31 @@ package com.interyouhunt.hunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InterviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interview);
+
+        TextView company = (TextView) findViewById(R.id.company);
+        String userCompany = "Google";
+        company.setText(userCompany);
+
+        TextView roundType = (TextView) findViewById(R.id.roundType);
+        String userRound = "Behavioral";
+        roundType.setText(userRound);
+
+        TextView recruiterEmail = (TextView) findViewById(R.id.recruiterEmail);
+        String userRecruitEmail  = "faisalgedi@gmail.com";
+        recruiterEmail.setText(userRecruitEmail);
+
+        TextView notes = (TextView) findViewById(R.id.notesText);
+        String userNotes  = "Aye let me tell you something she asked me why I wanna work at Google? I acted like it wasn't for the bread.";
+        notes.setText(userNotes);
+
     }
 }
