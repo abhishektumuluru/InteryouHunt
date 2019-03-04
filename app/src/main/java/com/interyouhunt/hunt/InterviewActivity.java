eff43ed562c1843690a6c55b1f01827aac08ccab
faisalgedi <faisalgedi@gmail.com>
2019-03-04 02:47:52 -0500

    nav bar

--- app/src/main/java/com/interyouhunt/hunt/InterviewActivity.java
+++ app/src/main/java/com/interyouhunt/hunt/InterviewActivity.java
@@ -1,7 +1,10 @@
 package com.interyouhunt.hunt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class InterviewActivity extends AppCompatActivity {
@@ -27,6 +30,33 @@ public class InterviewActivity extends AppCompatActivity {
         TextView notes = findViewById(R.id.notesText);
         String userNotes  = "Aye let me tell you something she asked me why I wanna work at Google? I acted like it wasn't for the bread.";
         notes.setText(userNotes);
+
+        Button home = findViewById(R.id.toHome);
+        home.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InterviewActivity.this, HomeActivity.class));
+            }
+        });
+
+        Button toDo = findViewById(R.id.toDo);
+        toDo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InterviewActivity.this, HomeActivity.class));
+            }
+        });
+
+        Button profile = findViewById(R.id.toProfile);
+        profile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InterviewActivity.this, ProfileActivity.class));
+            }
+        });
 
     }
 }
